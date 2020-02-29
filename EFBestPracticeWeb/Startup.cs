6a5410476d04df3d34a90ae507729f05ee491d09
Nguyen Quoc Cuong <ngquoccuong998@gmail.com>
2020-02-29 23:21:18 +0700

    add migration

--- EFBestPracticeWeb/Startup.cs
+++ EFBestPracticeWeb/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using EFDataAccessLibrary.DataAccess;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFBestPracticeWeb
 {
@@ -23,6 +25,10 @@ namespace EFBestPracticeWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PeopleContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("Default"));
+            });
             services.AddRazorPages();
         }
 
---
+++ EFDataAccessLibrary/DataAccess/PeopleContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDataAccessLibrary.DataAccess
+{
+    public class PeopleContext : DbContext
+    {
+        public PeopleContext(DbContextOptions options) : base(options) { }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Email> Emails { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
